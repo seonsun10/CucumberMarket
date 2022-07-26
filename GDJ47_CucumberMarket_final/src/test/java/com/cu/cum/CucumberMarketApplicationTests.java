8da1a0c9d996bbd87cde44f47d831eb4b6e04026
tjsdn <dbxlf3018@gmail.com>
2022-07-26 16:53:20 +0900

    finalproject

---
+++ GDJ47_CucumberMarket_final/src/test/java/com/cu/cum/CucumberMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cu.cum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
