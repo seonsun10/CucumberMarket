28f662e23fcf5afff8a35f4013f5191380075f27
KDH <KDH@INNO-A-870>
2022-08-18 12:07:10 +0900

    시큐리티

--- GDJ47_CucumberMarket_final/src/main/java/com/cu/cum/config/SecurityConfig.java
+++ GDJ47_CucumberMarket_final/src/main/java/com/cu/cum/config/SecurityConfig.java
@@ -16,60 +16,60 @@ import org.springframework.security.web.firewall.HttpFirewall;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
-	public void configure(WebSecurity web) throws Exception {
-	    web.httpFirewall(defaultHttpFirewall());
-	}
-	 
-	@Bean
-	public HttpFirewall defaultHttpFirewall() {
-	    return new DefaultHttpFirewall();
-	}
-	
-	//시큐리티 설정 클래스
-	@Bean
-	public AuthenticationProvider authenticationProvider() {
-		return new MyProvider();
-	}
-	
-	@Bean
-	public BCryptPasswordEncoder bcryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-	
-	@Bean
-	public SecurityFilterChain authenticatePath(HttpSecurity http) throws Exception{
-		return http.csrf().disable()
-				.formLogin()
-					.loginPage("/loginpage")
-					.loginProcessingUrl("/loginprocess")
-					.successForwardUrl("/loginsuccess")
-					.usernameParameter("userId")
-					.passwordParameter("password")
-					.and() //http부터 또 설정 가능
+   
+   public void configure(WebSecurity web) throws Exception {
+       web.httpFirewall(defaultHttpFirewall());
+   }
+    
+   @Bean
+   public HttpFirewall defaultHttpFirewall() {
+       return new DefaultHttpFirewall();
+   }
+   
+   //시큐리티 설정 클래스
+   @Bean
+   public AuthenticationProvider authenticationProvider() {
+      return new MyProvider();
+   }
+   
+   @Bean
+   public BCryptPasswordEncoder bcryptPasswordEncoder() {
+      return new BCryptPasswordEncoder();
+   }
+   
+   @Bean
+   public SecurityFilterChain authenticatePath(HttpSecurity http) throws Exception{
+      return http.csrf().disable()
+            .formLogin()
+               .loginPage("/loginpage")
+               .loginProcessingUrl("/loginprocess")
+               .successForwardUrl("/loginsuccess")
+               .usernameParameter("userId")
+               .passwordParameter("password")
+               .and() //http부터 또 설정 가능
 
-				.authorizeRequests() //인증 권한
-					.antMatchers("/member/*","/inquiryList","/reportList","/testchat.do/**","/wishlist/**").hasAnyRole("USER","ADMIN")
-					.antMatchers("/board/*").hasAnyRole("USER","ADMIN")
+            .authorizeRequests() //인증 권한
+               .antMatchers("/member/*","/inquiryList","/reportList","/testchat.do/**").hasAnyRole("USER","ADMIN")
+               .antMatchers("/board/*").hasAnyRole("USER","ADMIN")
 
-					.antMatchers("/**","/resources/**","/member/selectNewChatCnt.do",
-						"/member/selectNewNoticeCnt.do").permitAll() //"/loginpage","/login","/joinForm","/join","/mailCheck","/idCheck", ,"/area.do"
-					.antMatchers("/admin").hasRole("ADMIN")
-					// /admin 요청에 대해서는 ROLE_ADMIN 역할을 가지고 있어야 함
-//	                .antMatchers("/admin").hasRole("ADMIN")
-	                // 나머지 요청에 대해서는 로그인을 요구하지 않음
-					.anyRequest().authenticated()
-					                //.anyRequest().permitAll()
-					.and()
-				.exceptionHandling().accessDeniedPage("/denie")
-					.and()
-				.logout()
-					.logoutUrl("/logout")
-					.logoutSuccessUrl("/successLogout.do")
-					.and()
-				
-				.authenticationProvider(authenticationProvider())//인증 대상 빈을 받아서 설정
-				.build();
-	}
-	
+               .antMatchers("/**","/resources/**","/member/selectNewChatCnt.do",
+                  "/member/selectNewNoticeCnt.do").permitAll() //"/loginpage","/login","/joinForm","/join","/mailCheck","/idCheck", ,"/area.do"
+               .antMatchers("/admin").hasRole("ADMIN")
+               // /admin 요청에 대해서는 ROLE_ADMIN 역할을 가지고 있어야 함
+//                   .antMatchers("/admin").hasRole("ADMIN")
+                   // 나머지 요청에 대해서는 로그인을 요구하지 않음
+               .anyRequest().authenticated()
+                               //.anyRequest().permitAll()
+               .and()
+            .exceptionHandling().accessDeniedPage("/denie")
+               .and()
+            .logout()
+               .logoutUrl("/logout")
+               .logoutSuccessUrl("/successLogout.do")
+               .and()
+            
+            .authenticationProvider(authenticationProvider())//인증 대상 빈을 받아서 설정
+            .build();
+   }
+   
 }
